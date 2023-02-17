5dc066461e1e9146a9383ab5f36bc325f0d75eec
VaishnaviM411 <vaishnaviv@sahaj.ai>
2023-02-17 11:12:52 +0530

    Reformatting Vehicle.kt

--- src/main/kotlin/model/Vehicle.kt
+++ src/main/kotlin/model/Vehicle.kt
@@ -6,17 +6,22 @@ class Vehicle {
     fun park(parkingLot: ParkingLot): Ticket {
         val spot = parkingLot.getNextAvailableSpot()
         spot?.book(this)
+
         val spotInParkingLot = parkingLot.spots.find{ it -> spot!! == it }
         parkingLot.spots[parkingLot.spots.indexOf(spotInParkingLot)] = spot!!
+
         parkingLot.decreaseAvailableSpots()
+
         return Ticket(spot.getSpotNumber())
     }
 
     fun unPark(parkingLot: ParkingLot, ticket: Ticket): Receipt {
         val spot = parkingLot.getSpotBySpotNumber(ticket.getSpotNumber())
         spot?.unBook()
+
         val spotInParkingLot = parkingLot.spots.find { it -> spot!! == it }
         parkingLot.spots[parkingLot.spots.indexOf(spotInParkingLot)] = spot!!
+
         parkingLot.increaseAvailableSpots()
 
         return Receipt(ticket, Fee())
