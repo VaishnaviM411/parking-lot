80fd7e238f2b1b6cc4ba08f9972fc407266533b4
VaishnaviM411 <vaishnaviv@sahaj.ai>
2023-02-19 09:24:38 +0530

    Change Ticket class to take entryDateTime as constructor parameter

--- src/main/kotlin/model/Ticket.kt
+++ src/main/kotlin/model/Ticket.kt
@@ -2,8 +2,7 @@ package model
 
 import java.time.LocalDateTime
 
-class Ticket(private val spotNumber: Int) {
-    private val entryDateTime: LocalDateTime = LocalDateTime.now()
+class Ticket(private val spotNumber: Int, private val entryDateTime: LocalDateTime = LocalDateTime.now()) {
     fun getSpotNumber() = spotNumber
 
     fun getEntryDateTime() = entryDateTime
--- src/test/kotlin/model/TicketTest.kt
+++ src/test/kotlin/model/TicketTest.kt
@@ -2,14 +2,17 @@ package model
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class TicketTest {
     @Test
     fun `It should return a ticket`() {
         val spotNumber = 1
+        val entryDateTime = LocalDateTime.now()
 
-        val response = Ticket(spotNumber)
+        val response = Ticket(spotNumber, entryDateTime)
 
         assertEquals(spotNumber, response.getSpotNumber())
+        assertEquals(entryDateTime, response.getEntryDateTime())
     }
 }
