1b4be5e1b20d4bee7db9c35206a53da4b7237905
VaishnaviM411 <vaishnaviv@sahaj.ai>
2023-02-19 18:37:53 +0530

    Add Main logic for park

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,49 @@
-fun main(args: Array<String>) {
-    println("Program arguments: ${args.joinToString()}")
+import model.ParkingLot
+import model.Ticket
+import java.time.LocalDateTime
+import java.util.UUID.randomUUID
+
+fun main() {
+    val parkingLot = ParkingLot(NUMBER_OF_SPOTS)
+
+    val actions = listOf("1. Park", "2. Unpark", "0. Exit")
+
+    actions.forEach { println(it) }
+
+    var selectedAction = readln().toInt()
+
+    while (selectedAction != 0) {
+
+        when (selectedAction) {
+            1 -> {
+                try {
+                    val spot = parkingLot.getNextAvailableSpot()
+                    val ticket = parkingLot.park(spot)
+                    println(ticket)
+                } catch (error: Error) {
+                    println(error)
+                } catch (exception: Exception){
+                    println(exception)
+                }
+            }
+
+            2 -> {
+                println("Enter spot number: ")
+//                var spotNumber = readln().toInt()
+                val spotNumber = randomUUID()
+                val ticket = Ticket(spotNumber, LocalDateTime.now().minusHours(4))
+                val receipt = parkingLot.unpark(ticket)
+                println(receipt)
+            }
+
+            else -> {
+                println("No action found!")
+            }
+        }
+
+        actions.forEach { println(it) }
+
+        selectedAction = readln().toInt()
+
+    }
 }
