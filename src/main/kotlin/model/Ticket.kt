976908e1d9e019aac9e0f71c5b8c87f2f456b15b
VaishnaviM411 <vaishnaviv@sahaj.ai>
2023-02-19 18:58:38 +0530

    Add toString method to Ticket

--- src/main/kotlin/model/Ticket.kt
+++ src/main/kotlin/model/Ticket.kt
@@ -1,9 +1,18 @@
 package model
 
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class Ticket(private val spotNumber: Int, private val entryDateTime: LocalDateTime = LocalDateTime.now()) {
     fun getSpotNumber() = spotNumber
 
     fun getEntryDateTime() = entryDateTime
+
+    override fun toString():String {
+        return """
+            Spot number: $spotNumber
+            \n
+            Entry time: ${entryDateTime.format(DateTimeFormatter.ISO_DATE)}
+        """.trimIndent()
+    }
 }
--- src/test/kotlin/model/TicketTest.kt
+++ src/test/kotlin/model/TicketTest.kt
@@ -3,6 +3,7 @@ package model
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class TicketTest {
     @Test
@@ -14,5 +15,20 @@ class TicketTest {
 
         assertEquals(spotNumber, response.getSpotNumber())
         assertEquals(entryDateTime, response.getEntryDateTime())
+    }
+
+    @Test
+    fun `It should return ticket info in string format`() {
+        val spotNumber = 1
+        val entryDateTime = LocalDateTime.now()
+        val ticket = Ticket(spotNumber, entryDateTime)
+
+        val response = ticket.toString()
+
+        assertEquals("""
+            Spot number: $spotNumber
+            \n
+            Entry time: ${entryDateTime.format(DateTimeFormatter.ISO_DATE)}
+        """.trimIndent(), response)
     }
 }
